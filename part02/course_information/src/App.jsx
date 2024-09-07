6ee2a2ac009211975f0c6c88e8f86aef026017e7
Markus Laaksonen <markus@mxa.fi>
2024-09-07 11:56:02 +0300

    part2 ex01

---
+++ part02/course_information/src/App.jsx
@@ -0,0 +1,65 @@
+const Header = (props) => {
+  return <h1>{props.course.name}</h1>
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      {props.course.parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.course.parts.reduce((acc, part) => acc + part.exercises, 0)}</p>
+  )
+}
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course} />
+      <Content course={props.course} />
+      <Total course={props.course} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  }
+
+  return <Course course={course} />
+}
+
+export default App
